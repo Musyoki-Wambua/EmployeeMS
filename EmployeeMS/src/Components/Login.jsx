2a075c7a7037b7704b0fcf15355aa8422486bf08
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-11-23 00:34:29 +0300

    Handle Input fields

--- EmployeeMS/src/Components/Login.jsx
+++ EmployeeMS/src/Components/Login.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
+import axios from 'axios'
 
 const Login = () => {
+  const[values, setValues] = useState({
+    email: '',
+    password:''
+  })
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    axios.post('http:localhost:3000/auth/adminlogin')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
+  
   return (
-    <div>
-      <div>
+    <div className='d-flex justify-content-center align-items-center vh-100 LoginPage'>
+      <div className='p-3 rounded w-25 border LoginForm'>
         <h2>Login Page</h2>
-        <form action="">
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" name='email' autoComplete='off'  placeholder='Enter Email' className='form-control rounded-0'/>
+        <form onSubmit={handleSubmit}>
+            <div className='mb-3'>
+                <label htmlFor="email"><strong>Email:</strong></label>
+                <input type="email" name='email' autoComplete='off'  placeholder='Enter Email' 
+                onChange={(e) => setValues({...values, email: e.target.value})} className='form-control rounded-0'/>
             </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name='password' autoComplete='off'  placeholder='Enter Password' className='form-control rounded-0'/>
+            <div className='mb-3'>
+                <label htmlFor="password"><strong>Password:</strong></label>
+                <input type="password" name='password' autoComplete='off'  placeholder='Enter Password' 
+                onChange={(e) => setValues({...values, password: e.target.value})} className='form-control rounded-0'/>
             </div>
-            <button className='btn btn-success w-100 rounded-0 '>Submit</button>            
+            <button className='btn btn-success w-100 rounded-0 mb-2 '>Log In</button>
+            <div className='mb-1'>
+              <input type="checkbox" name='tick' id='tick' className='me-2' />
+              <label htmlFor="checkbox">You Agree With Our Terms & Conditions</label>
+            </div>            
         </form>
       </div>
     </div>
   )
 }
+
 
 export default Login
