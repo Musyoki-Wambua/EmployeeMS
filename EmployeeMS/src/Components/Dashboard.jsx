f616f25a36a987cdd864528820bf41cf1df9b725
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-11-25 08:10:51 +0300

    Added Navbar

--- EmployeeMS/src/Components/Dashboard.jsx
+++ EmployeeMS/src/Components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 const Dashboard = () => {
@@ -49,6 +49,12 @@ const Dashboard = () => {
                     </li>                    
                 </ul>
             </div>
+        </div>
+        <div className='col p-0 m-0'>
+            <div className='p-2 d-flex justify-content-center shadow'>
+                <h4>Employee Management Sysytem</h4>
+            </div>
+            <Outlet />
         </div>
       </div>
     </div>
