24c47c1e07f7ba0f220f96dfb686461ee0d71ec3
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-11-25 20:57:42 +0300

    Edit Employee Component done

--- EmployeeMS/src/Components/EditEmployee.jsx
+++ EmployeeMS/src/Components/EditEmployee.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 
 const EditEmployee = () => {
@@ -8,12 +8,12 @@ const EditEmployee = () => {
     const[employee, setEmployee] = useState({
         name: '',
         email: '',
-        password: '',
         salary: '',
         address: '',
         category_id: '',
         image: ''
     });
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get('http://locakhost:3000/auth/category')
@@ -28,15 +28,35 @@ const EditEmployee = () => {
 
         axios.get('http://locakhost:3000/auth/employee' + id)
         .then(result => {
-           console.log(result.data)
+           setEmployee({
+                ...employee,
+                name: result.data.Result[0].name,
+                email: result.data.Result[0].email,
+                address: result.data.Result[0].address,
+                salary: result.data.Result[0].salary,
+                image: result.data.Result[0].image,
+                category_id: result.data.Result[0].category_id
+           })
           }).catch(err => console.log(err))
       }, []);
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.put('http://localhost:3000/auth/edit_employee' + id, employee)
+        .then(result => {
+            if(result.data.Status){
+                navigate('/dashboard/employee')
+            }else{
+                alert(result.data.Error)
+            }
+        }).catch((err) => console.log(err))
+      }
 
   return (
     <div className="d-flex justify-content-center align-items-center mt-3">
     <div className="p-3 rounded w-50 border">
       <h3 className="text-center">Edit Employee</h3>
-      <form className="row g-1" >
+      <form className="row g-1" onSubmit={handleSubmit} >
         <div className="col-12">
           <label htmlFor="inputName" className="form-label">
             Name
@@ -46,6 +66,7 @@ const EditEmployee = () => {
             className="form-control rounded-0"
             id="inputName"
             placeholder="Enter Name"
+            value={employee.name}
             onChange={(e) => setEmployee({...employee, name: e.target.value})}
           />
         </div>
@@ -59,30 +80,8 @@ const EditEmployee = () => {
             id="inputEmail4"
             placeholder="Enter Email"
             autoComplete="off"
+            value={employee.email}
             onChange={(e) => setEmployee({...employee, email: e.target.value})}
-          />
-        </div>
-        <div className="col-12">
-          <label htmlFor="inputPassword4" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control rounded-0"
-            id="inputPassword4"
-            placeholder="Enter Password"
-            onChange={(e) => setEmployee({...employee, password: e.target.value})}
-          />
-          <label htmlFor="inputSalary" className="form-label">
-            Salary
-          </label>
-          <input
-            type="text"
-            className="form-control rounded-0"
-            id="inputSalary"
-            placeholder="Enter Salary"
-            autoComplete="off"
-            onChange={(e) => setEmployee({...employee, salary: e.target.value})}
           />
         </div>
         <div className="col-12">
@@ -95,7 +94,22 @@ const EditEmployee = () => {
             id="inputAddress"
             placeholder="1234 EastLands Nairobi"
             autoComplete="off"
+            value={employee.address}
             onChange={(e) => setEmployee({...employee, address: e.target.value})}
+          />
+        </div>
+        <div className="col-12">
+          <label htmlFor="salary" className="form-label">
+            Salary
+          </label>
+          <input
+            type="integer"
+            className="form-control rounded-0"
+            id="salary"
+            placeholder="Enter Salary"
+            autoComplete="off"
+            value={employee.salary}
+            onChange={(e) => setEmployee({...employee, salary: e.target.value})}
           />
         </div>
         <div className="col-12">
@@ -118,12 +132,13 @@ const EditEmployee = () => {
             className="form-control rounded-0"
             id="inputGroupFile01"
             name="image"
+            value={employee.image}
             onChange={(e) => setEmployee({...employee, image: e.target.files[0]})}
           />
         </div>
         <div className="col-12">
           <button type="submit" className="btn btn-primary w-100">
-            Add Employee
+            Edit Employee
           </button>
         </div>
       </form>
